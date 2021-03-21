11045db7c463371388486eed5a5146d9c06df935
teminoah <dropurmensaje@gmail.com>
2021-03-21 16:56:47 +0100

    exposed APIs to create actors

---
+++ src/main/java/com/studomia/studomia/StudomiaApplication.java
@@ -0,0 +1,13 @@
+package com.studomia.studomia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudomiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudomiaApplication.class, args);
+	}
+
+}
