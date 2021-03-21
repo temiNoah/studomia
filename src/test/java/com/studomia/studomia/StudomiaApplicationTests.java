be81141ea3e44e678c8fb6d1bd3295eed326da10
teminoah <dropurmensaje@gmail.com>
2021-03-21 17:02:06 +0100

    exposed APIs to create actors

---
+++ src/test/java/com/studomia/studomia/StudomiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studomia.studomia;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudomiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
